e85f8a951fbe902d147c811876a4f9d7263d354d
Minh Nghia Duong <minhnghiaduong997@gmail.com>
2020-05-20 16:50:59 +0200

    add description of election

--- core/libapg_qt/Election/election_manager.h
+++ core/libapg_qt/Election/election_manager.h
@@ -21,7 +21,33 @@ namespace AirPlug
 {
 
 /**
- * @brief The ElectionManager class manage election processes of a site
+ * @brief The ElectionManager class manages election processes of a site
+ * Election is used for chosing a unique initiator in a use case where the network need a unique initiator.
+ * At every point in time, there is always at most one ongoing election for each reason
+ *
+ * The mechanism of election in a network is described as below:
+ *
+ *  - When ever a site need to call for an election, it check if there is already an ongoing election for the same reason,
+ *      + if not, it will save information of the election and broadcast election message to all sites
+ *      + if there already is an election, it backs down
+ *
+ *  - When a site receive an election message, it will update information about that election in the list of ongoing elections:
+ *      + If the siteID of the sender is smaller that the siteID of the currect candidate, it will replace the current candidate with the siteID of sender.
+ *        Then send back an ACK Message with the siteID of the current candidate
+ *      + If the siteID of sender is greater, it will keep the current candidate at the one it vote for
+ *        and send back an ACK message with the siteID of the current candidate
+ *
+ *      => Therefore, in the case where there are multiple candidates enter the election process,
+ *         there is always only one candidate who receives the accord from all site, this one will be decided to win the election
+ *
+ *  - When a site receive an ACK message, it update the information about the election by:
+ *      + Decrement the number of waiting responses
+ *      + See if ACK message is vote for who, update the number of FOR and AGAINST
+ *      + When the number of waiting responses reachs 0, if the number of FOR vote is equal to the number of sites then it wins the election
+ *
+ *  - When a site wins an election, it can enter a demanding task where it becomes the initiator.
+ *    When the task is finished, the site broadcast out a FINISH_ELECTION message to inform all sites that the election is finished
+ *    and it can be remove from the list of ongoing elections
  */
 class ElectionManager : public QObject
 {
