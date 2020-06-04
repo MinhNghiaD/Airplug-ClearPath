936365704b0c11eaec2c711262bc63c4af0df50a
Minh Nghia Duong <minhnghiaduong997@gmail.com>
2020-06-04 09:07:28 +0200

    propotype environment manager

---
+++ core/extras/clearpath/environment_manager.h
@@ -0,0 +1,13 @@
+#ifndef ENVIRONMENT_MANAGER_H
+#define ENVIRONMENT_MANAGER_H
+
+namespace ClearPath
+{
+
+class EnvironmentManager
+{
+public:
+    EnvironmentManager();
+};
+}
+#endif // ENVIRONMENT_MANAGER_H
